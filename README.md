c5fec20cb943a8c37bfbf4b0871932eb75bdf975
nitram509 <maki@bitkings.de>
2014-08-23 15:46:36 +0200

    added example to README

--- README.md
+++ README.md
@@ -6,6 +6,26 @@ authorization tokens that work great in distributed systems.  Like cookies,
 macaroons are bearer tokens that enable applications to ascertain whether their
 holders' actions are authorized.  But macaroons are better than cookies!
 
+Creating Your First Macaroon
+----------------------------------
+
+Lets create a simple macaroon:
+````
+String location = "http://www.example.org";
+String secretKey = "this is our super secret key; only we should know it";
+String identifier = "we used our secret key";
+Macaroon macaroon = MacaroonsBuilder.create(location, secretKey, identifier);
+System.out.println(macaroon.inspect());
+````
+
+````
+location http://www.example.org
+identifier we used our secret key
+signature e3d9e02908526c4c0039ae15114115d97fdd68bf2ba379b342aaf0f617d0552f
+````
+
+
+
 Build Status
 --------------------
 
--- src/example/java/com/github/nitram509/jmacaroons/examples/MacaroonsExamples.java
+++ src/example/java/com/github/nitram509/jmacaroons/examples/MacaroonsExamples.java
@@ -3,12 +3,11 @@ package com.github.nitram509.jmacaroons.examples;
 import com.github.nitram509.jmacaroons.Macaroon;
 import com.github.nitram509.jmacaroons.MacaroonsBuilder;
 
+/**
+ * These are examples, for copy&paste into README.md.
+ * They should be correct and compilable.
+ */
 public class MacaroonsExamples {
-
-  public static void main(String[] args) {
-    MacaroonsExamples examples = new MacaroonsExamples();
-    examples.create();
-  }
 
   public void create() {
     String location = "http://www.example.org";
@@ -16,6 +15,11 @@ public class MacaroonsExamples {
     String identifier = "we used our secret key";
     Macaroon macaroon = MacaroonsBuilder.create(location, secretKey, identifier);
     System.out.println(macaroon.inspect());
+  }
+
+  public static void main(String[] args) {
+    MacaroonsExamples examples = new MacaroonsExamples();
+    examples.create();
   }
 
 }
