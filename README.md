9859f374f498294ce6ddcb6de012c0173be5a024
nitram509 <maki@bitkings.de>
2014-08-23 15:54:14 +0200

    added more examples to README

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ authorization tokens that work great in distributed systems.  Like cookies,
 macaroons are bearer tokens that enable applications to ascertain whether their
 holders' actions are authorized.  But macaroons are better than cookies!
 
+
 Creating Your First Macaroon
 ----------------------------------
 
@@ -24,6 +25,29 @@ identifier we used our secret key
 signature e3d9e02908526c4c0039ae15114115d97fdd68bf2ba379b342aaf0f617d0552f
 ````
 
+
+Serializing
+----------------------------------
+
+````
+String serialized = macaroon.serialize();
+System.out.println("Serialized: " + serialized);
+````
+
+````
+Serialized: MDAyNGxvY2F0aW9uIGh0dHA6Ly93d3cuZXhhbXBsZS5vcmcKMDAyNmlkZW50aWZpZXIgd2UgdXNlZCBvdXIgc2VjcmV0IGtleQowMDJmc2lnbmF0dXJlIOPZ4CkIUmxMADmuFRFBFdl/3Wi/K6N5s0Kq8PYX0FUvCg==
+````
+
+
+Verifying Your Macaroon
+----------------------------------
+
+````
+MacaroonsVerifier verifier = new MacaroonsVerifier();
+String secret = "this is our super secret key; only we should know it";
+boolean valid = verifier.verify(macaroon, secret);
+System.out.println("Macaroon is " + (valid ? "Valid" : "Invalid"));
+````
 
 
 Build Status
--- src/example/java/com/github/nitram509/jmacaroons/examples/MacaroonsExamples.java
+++ src/example/java/com/github/nitram509/jmacaroons/examples/MacaroonsExamples.java
@@ -2,6 +2,10 @@ package com.github.nitram509.jmacaroons.examples;
 
 import com.github.nitram509.jmacaroons.Macaroon;
 import com.github.nitram509.jmacaroons.MacaroonsBuilder;
+import com.github.nitram509.jmacaroons.MacaroonsVerifier;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * These are examples, for copy&paste into README.md.
@@ -9,17 +13,41 @@ import com.github.nitram509.jmacaroons.MacaroonsBuilder;
  */
 public class MacaroonsExamples {
 
-  public void create() {
+  private Macaroon create() {
     String location = "http://www.example.org";
     String secretKey = "this is our super secret key; only we should know it";
     String identifier = "we used our secret key";
     Macaroon macaroon = MacaroonsBuilder.create(location, secretKey, identifier);
     System.out.println(macaroon.inspect());
+
+    return macaroon;
+  }
+
+  private void serialize() {
+    Macaroon macaroon = create();
+
+    String serialized = macaroon.serialize();
+    System.out.println("Serialized: " + serialized);
+  }
+
+  private void verify() throws InvalidKeyException, NoSuchAlgorithmException {
+    Macaroon macaroon = create();
+
+    MacaroonsVerifier verifier = new MacaroonsVerifier();
+    String secret = "this is our super secret key; only we should know it";
+    boolean valid = verifier.verify(macaroon, secret);
+    System.out.println("Macaroon is " + (valid ? "Valid" : "Invalid"));
   }
 
   public static void main(String[] args) {
     MacaroonsExamples examples = new MacaroonsExamples();
-    examples.create();
+    try {
+      examples.create();
+      examples.serialize();
+      examples.verify();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
