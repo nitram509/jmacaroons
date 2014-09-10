b782501929432ff5499236b2b2c9002a767ecf3c
nitram509 <maki@bitkings.de>
2014-09-10 23:41:51 +0200

    more renamings

--- src/test/java/com/github/nitram509/jmacaroons/util/BinHexTest.java
+++ src/test/java/com/github/nitram509/jmacaroons/util/BinHexTest.java
@@ -20,25 +20,25 @@ import org.testng.annotations.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-public class HexTest {
+public class BinHexTest {
 
   @Test
   public void happy_path() {
-    String hexstr = Hex.toHex((byte) 1, (byte) 128, (byte) 255);
+    String hexstr = BinHex.bin2hex((byte) 1, (byte) 128, (byte) 255);
 
     assertThat(hexstr).isEqualTo("0180ff");
   }
 
   @Test
   public void null_safe() {
-    String hexstr = Hex.toHex(null);
+    String hexstr = BinHex.bin2hex(null);
 
     assertThat(hexstr).isNull();
   }
 
   @Test
   public void empty_string() {
-    String hexstr = Hex.toHex();
+    String hexstr = BinHex.bin2hex();
 
     assertThat(hexstr).isEqualTo("");
   }
