5b6edd344cb4577f239254de180bfe26c84b17b6
nitram509 <maki@bitkings.de>
2014-10-14 20:50:25 +0200

    fixed copyright

--- src/test/java/com/github/nitram509/jmacaroons/MacaroonsPrepareRequestAndVerifyTest.java
+++ src/test/java/com/github/nitram509/jmacaroons/MacaroonsPrepareRequestAndVerifyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright $todaz.zear Martin W. Kirst
+ * Copyright 2014 Martin W. Kirst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
