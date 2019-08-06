b3c347cc670e0b10fcc679f5f2a701b395a73ea9
Jason Axley <jaxley@expedia.com>
2019-08-06 10:37:40 -0700

    Fixed example code bug and added unit test to catch future broken examples.  Replaced hard-coded future dates with dynamic dates to avoid additional timebombs.

---
+++ src/test/java/com/github/nitram509/jmacaroons/examples/MacaroonsExamplesTest.java
@@ -0,0 +1,12 @@
+package com.github.nitram509.jmacaroons.examples;
+
+import org.testng.annotations.Test;
+
+public class MacaroonsExamplesTest {
+
+  @Test
+  public void testExamplesRunWithoutErrors() {
+    MacaroonsExamples.main(new String[0]);
+  }
+
+}
